4a0cc75d59ee50a14f5601369c42a1e06b3c9c75
Jared Fraser <xanatosredfern@gmail.com>
2013-01-16 21:35:21 +1100

    Tidied up layout

--- spec/requests/categorisation_spec.rb
+++ spec/requests/categorisation_spec.rb
@@ -15,14 +15,33 @@ describe 'Categorising a Contact' do
     contact.reload.tag_names.map(&:to_s).should include("bad news")
   end
 
-  it "can list a bunch of taggings" do
-    contact = Watchtower::Contact.create!(first_name: "Jarold", last_name: "Frasarian")
-    contact.tag_with("some tag")
 
-    visit wtapp.tags_path
+  describe 'showing tags' do
+    context 'tags index' do 
+      it "can list a bunch of taggings" do
+        contact = Watchtower::Contact.create!(first_name: "Jarold", last_name: "Frasarian")
+        contact.tag_with("some tag")
 
-    click_link "some tag"
+        visit wtapp.tags_path
 
-    page.should have_content("Jarold Frasarian")
+        click_link "some tag"
+
+        page.should have_content("Jarold Frasarian")
+      end
+    end
+
+    context 'contact show' do
+      it "shows tags it has been tagged with" do
+        contact = Watchtower::Contact.create!(first_name: "Jarold", last_name: "Frasarian")
+        contact.tag_with("another tag")
+
+        visit wtapp.contact_path(contact)
+
+        within('.tags') do
+          page.should have_content("another tag")
+        end
+      end
+    end
   end
+
 end
